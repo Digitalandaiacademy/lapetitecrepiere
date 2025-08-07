b02ac8203d289fcd38617def407c5801d5722727
Digitalandaiacademy <obahjosephchanel@gmail.com>
2025-08-07 12:25:01 +0100

    Fix TypeScript errors and module imports for Netlify build

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -3,19 +3,10 @@ import { cn } from "@/lib/utils";
 
 const Card = React.forwardRef<
   HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement> & { asChild?: boolean }
->(({ className, asChild = false, ...props }, ref) => {
-  const Comp = asChild ? React.Fragment : "div";
-
-  // Si c’est un Fragment, on ne passe pas le ref
-  if (asChild) {
-    return (
-      <Comp {...props} className={cn("rounded-lg border border-gray-200 bg-white shadow-sm", className)} />
-    );
-  }
-
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => {
   return (
-    <Comp
+    <div
       ref={ref}
       {...props}
       className={cn("rounded-lg border border-gray-200 bg-white shadow-sm", className)}
